a0747f5288e198bf4e069aa6681eee4c65c7ca00
Kone <kone@gmail.com>
2022-03-23 22:41:12 +0100

    kone

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+       <h1>Hello tout le mone j'espere que vous allez bien !</h1>
+        <img src="images/m1.jpg" alt="une_image" width={200}   />
+       </div>
+       <div>
+       <h4>Hello tout le mone j'espere que vous allez bien !</h4>
+        <img src="images/m1.jpg" alt="une_image"  width={200}   />
+       </div>
+       <div>
+       <p>Hello tout le mone j'espere que vous allez bien !</p>
+        <img src="images/m1.jpg" alt="une_image"  width={200}  />
+       </div>
+       <div>
+       <span>Hello tout le mone j'espere que vous allez bien !</span>
+        <img src="images/m1.jpg" alt="une_image"  width={200}  />
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
